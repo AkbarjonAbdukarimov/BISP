860580bfe69e6940b1874ded1fbe425e0d183c7f
Akbarjon Abdukarimov <100358259+AkbarjonAbdukarimov@users.noreply.github.com>
2023-04-25 18:43:11 +0500

    set up successfull dev env

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -3,7 +3,10 @@ import Link from "next/link";
 export default function Home({ currentUser }) {
   return (
     <>
-      <h1>Welcome Back {currentUser && currentUser.email}</h1>
+      <div className="container">
+
+        <h1>Welcome Back {currentUser && currentUser.email}</h1>
+      </div>
     </>
   )
 }
