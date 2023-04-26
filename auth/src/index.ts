1a5d9f2412e7b586eb7861442813ab18378018d7
Akbarjon Abdukarimov <100358259+AkbarjonAbdukarimov@users.noreply.github.com>
2023-04-26 17:26:49 +0500

    fixed auth start script

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
-
+import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config();
 import { app } from "./app";
 
 const start = async () => {
