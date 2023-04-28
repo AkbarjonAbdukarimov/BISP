974ed358baad54d96f7967bd0b97dadc60c3b61b
Akbarjon Abdukarimov <100358259+AkbarjonAbdukarimov@users.noreply.github.com>
2023-04-28 18:34:11 +0500

    removed comment

--- posts/build/index.js
+++ posts/build/index.js
@@ -36,7 +36,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
-const dotenv = __importStar(require("dotenv")); // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+const dotenv = __importStar(require("dotenv"));
 dotenv.config();
 const app_1 = require("./app");
 const natsClinet_1 = __importDefault(require("./natsClinet"));
--- posts/src/index.ts
+++ posts/src/index.ts
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+import * as dotenv from "dotenv";
 dotenv.config();
 import { app } from "./app";
 import natsClient from "./natsClinet";
